a51adbcbfb6d7c8dfb7565d84f2bf1bd1409a964
RazMKhitaryan <razo96@mail.ru>
2020-11-02 21:33:23 +0400

    first commit

---
+++ Lesson1/src/picsart/Lesson1.java
@@ -0,0 +1,53 @@
+package picsart;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.print("Hello Java Lesson1");
+        System.out.println(" 2020");
+        System.out.println("PicsArt");
+
+        byte x = 78;
+        System.out.println(x);
+        x = -45;
+
+        short a = 1000;
+        System.out.println(a);
+
+        int q = 458987;
+        System.out.println(q);
+
+        long b = 87965465464546546L;
+        System.out.println(b);
+
+        double dd = 891.3;
+        System.out.println(dd);
+
+        float gg = 889.458f;
+        System.out.println(gg);
+
+        boolean mn = true;
+        boolean nm = false;
+        System.out.println(mn);
+        System.out.println(nm);
+
+        char ccc = 'r';
+        char accc = '#';
+        System.out.println(ccc);
+
+        char ppp = '\u0021';
+        char nnn = '\u0a23';
+        System.out.println(ppp);
+        System.out.println(nnn);
+
+        String name = "Raz";
+        System.out.println(name);
+        System.out.println(name + ppp + ccc + mn + " barev");
+    }
+}
+
+
+//CTRL+SPACE - Help
+//sout = System.out.println()
+//CTRL + D - Duplicate
+//ALT + CTRL + L = code styling
+//ALT +ENTER -
