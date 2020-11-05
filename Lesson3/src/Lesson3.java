c9ded5b8c816ab7937fbeb4711128be219947ca7
Razmik <73659794+RazMKhitaryan@users.noreply.github.com>
2020-11-05 18:10:09 +0400

    Update Lesson3.java

--- Lesson3/src/Lesson3.java
+++ Lesson3/src/Lesson3.java
@@ -9,12 +9,11 @@ public class Lesson3 {
         System.out.println();
 
 
+        int[] array = new int[6];
+        for (int i = 0; i < array.length; i++) {
 
-        int[] Array = new int[6];
-        for (int i = 0; i <Array.length ; i++) {
-
-            Array[i] = 2;
-            System.out.print(Array[i] + " ");
+            array[i] = 2;
+            System.out.print(array[i] + " ");
         }
         System.out.println();
         System.out.print("----------------Task2---------------");
@@ -24,65 +23,63 @@ public class Lesson3 {
 
         // Task 2 : Create an array and fill it with numbers from 1:1000.
 
-        int[] Array2 = new int[1001];
-        for (int i = 1; i < Array2.length; i++) {
-            System.out.print(i + " ");
+        int[] array2 = new int[1000];
+        for (int i = 0, j = 1; i < array2.length; i++, j++) {
+            array2[i] = j;
+            System.out.print(array2[i] + " ");
         }
         System.out.println();
         System.out.print("----------------Task3---------------");
         System.out.println();
-//----------------------------------------------
-
-
+////----------------------------------------------
 
         // Task 3 : Create an array and fill it with odd numbers from -20:20
 
-        int[] Array3 = new int[20];
-        for (int i = -19; i <= Array3.length; i += 2) {
-            System.out.print(i + " ");
+        int[] array3 = new int[20];
+        for (int i = 0, j = -19; i < array3.length; i++, j += 2) {
+            array3[i] = j;
 
+            System.out.print(array3[i] + " ");
         }
         System.out.println();
         System.out.print("----------------Task4---------------");
         System.out.println();
 //----------------------------------------------
 
-
         //Task 4 : Create an array and fill it. Print all elements which can be divided by 5.
 
-        int[] Array4 = new int[30];
-        for (int i = 0; i <= Array4.length; i++) {
-            if (i % 5 == 0) {
-                System.out.print(i + " ");
-            } else {
-                System.out.print(".");
+        int[] array4 = {2, 9, 8, 50, 0, 2, 3, 55, 75, 1, 2, 3, 6, 8, 9, 15, 60};
+        for (int i = 0; i < array4.length; i++) {
+            if (array4[i] % 5 == 0) {
+                System.out.print(array4[i] + " ");
             }
         }
         System.out.println();
         System.out.print("----------------Task5---------------");
         System.out.println();
-//----------------------------------------------
-
+////----------------------------------------------
 
         //Task 5 : Create an array and fill it. Print all elements which are between 24.12 and 467.23
-        double[] Array5 = new double[468];
-        for (double i = 24.12; i <= Array5.length; i = i + 0.01) {
+        double[] array5 = {26.32, 13.356, 500.236, 96.657, 32.458, 435.351};
+        for (int i = 0; i < array5.length; i++) {
+            if (array5[i] > 24.12 && array5[i] < 467.23) {
+                System.out.print(array5[i] + " ");
+            }
 
-            System.out.print(i + " ");
+
         }
         System.out.println();
         System.out.print("----------------Task6---------------");
         System.out.println();
-//----------------------------------------------
-
+////----------------------------------------------
 
         //Task 6 : Create an array and fill it. Print count of elements which can be divided by 2
 
-        int[] Array6 = new int[10];
+        int[] array6 = new int[10];
         int count = 0;
-        for (int i = 0; i <= Array6.length; i++) {
-
-            if (i % 2 == 0) {
+        for (int i = 0, j = 1; i < array6.length; i++, j++) {
+            array6[i] = j;
+            if (array6[i] % 2 == 0) {
                 count++;
             }
         }
@@ -90,7 +87,7 @@ public class Lesson3 {
         System.out.println();
         System.out.print("----------------Task7---------------");
         System.out.println();
-//----------------------------------------------
+////----------------------------------------------
 
         //Task 7 : Given an integer, 0< N < 21 , print its first 10 multiples.
         //Each multiple  N x i (where 0<i<11) should be printed on a new line in the form: N x i = result.
@@ -100,9 +97,6 @@ public class Lesson3 {
 
         }
         System.out.println();
-
-
-
 
 
     }
